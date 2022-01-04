b6bfb838d4e21bc15c854ddca8325f4843651a62
zekebaldwin <96504519+zekebaldwin@users.noreply.github.com>
2022-01-04 16:29:03 -0500

    Add files via upload

---
+++ destructturing.js
@@ -0,0 +1,37 @@
+1.) 
+    a.)8
+    b.)1846
+
+
+2.){yearND: 1846, yearMD: 1659}
+
+3.)
+    a.)your name is alejandro and you like purple
+    b.)your name is melissa and you like green
+    c.)your name is undefined and you like green
+
+4.)
+    a.)maya 
+    b.)marissa
+    c.)chi
+
+5.)
+    a.)raindrops on roses
+    b.)whiskers on kittens 
+    c.)[bright copper kettles, warm woolen mittens, brown paper packages tied up with strings]
+
+6.)[10, 30, 20]
+
+7.) const {a, b} = obj.numbers
+
+8.) [arr[0], arr[1]] = [arr[1], arr[0]]
+
+9.)
+    const raceResults = (arr) => {
+        return {
+            first,
+            second,
+            third,
+            ...rest
+        }
+    }
